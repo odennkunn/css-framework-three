f249020c7745e4ee050912d24242b3d4c5c9ccfc
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-10-15 10:18:44 +0900

    chakra-ui-test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Stack, Flex, Box, Grid, Heading, Button } from "@chakra-ui/core";
 
-function App() {
+const App = () => {
+  let [boxHeight, editHeight] = useState(20);
+  let [boxColor, editColor] = useState('red');
+  let [colorIntensity, editIntensity] = useState(500);
+
+  const boxSettings = {
+    flex: "1",
+    textAlign: "center",
+    color: "white",
+    h: `${boxHeight}px`,
+    bg: `${boxColor}.${colorIntensity}`
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box {...boxSettings}>I'm a Box</Box>
+      <Flex justifyContent="space-around">
+        <Stack>
+          <Heading size="md">Size</Heading>
+          <Button variantColor="red" onClick={() => editHeight(boxHeight -= 10)} border="none">Shrink</Button>
+          <Button variantColor="green" onClick={() => editHeight(boxHeight += 10)} border="none">Grow</Button>
+        </Stack>
+
+        <Stack>
+          <Heading size="md">Color</Heading>
+          <Flex w="200px" justifyContent="space-between">
+            <Stack>
+              <Button variantColor="green" onClick={() => editColor('green')} border="none">Green</Button>
+              <Button variantColor="blue" onClick={() => editColor('blue')} border="none">Blue</Button>
+              <Button variantColor="red" onClick={() => editColor('red')} border="none">Red</Button>
+            </Stack>
+
+            <Stack>
+              <Button variantColor="gray" variant="outline" onClick={() => editIntensity(colorIntensity -= 100)} border="none">Lighter</Button>
+              <Button variantColor="gray" variant="outline" onClick={() => editIntensity(colorIntensity += 100)} border="none">Darker</Button>
+            </Stack>
+          </Flex>
+        </Stack>
+      </Flex>
     </div>
   );
-}
+};
+
 
 export default App;
